9e7ec67a2554ec1e3a75f12cea8784dd17de02c7
Mack Hashemi <mack@hashemi.com>
2015-10-20 13:01:18 -0700

    Initial submit
    Initial submit

---
+++ LinkedList.h
@@ -0,0 +1,34 @@
+/** @file LinkedList.h */
+#ifndef _LINKED_LIST
+#define _LINKED_LIST
+
+#include "ListInterface.h"
+#include "Node.h"
+
+template <class ItemType>
+
+class LinkedList : public ListInterface <ItemType>
+{
+private:
+    Node <ItemType> item;
+    Node <ItemType>* next;
+    Node <ItemType>* prev;
+
+public:
+
+    LinkedList(); // Default constructor
+    virtual ~LinkedList(); // Destructor
+
+    // Linked List Operations
+    bool isEmpty() const;
+    int getLength() const;
+    bool insert(int newPosition, const ItemType& newEntry);
+    bool remove(int position);
+    void clear();
+    ItemType getEntry(int position) const;
+    void setEntry(int position, const ItemType& newEntry);
+
+}; // end LinkedList
+
+#include "LinkedList.cpp"
+#endif // _LINKED_LIST
---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+#include "Stack.h"
+
+using namespace std;
+
+int main(void)
+{
+
+  //int i;
+
+  Stack<int> stack;
+
+#if 0
+  s.push(6);
+  s.push(2);
+  s.push(3);
+
+  i = s.peek();
+  cout << "peek 1 is " << i << endl;
+
+  s.pop();
+
+  i = s.peek();
+  cout << "peek 2 is " << i << endl;
+
+  s.pop();
+
+  i = s.peek();
+  cout << "peek 3 is " << i << endl;
+#endif
+
+  return 0;
+}
